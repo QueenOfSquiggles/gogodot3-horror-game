f9cd6e810362d1fb162785389078d0cea47442b7
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2022-06-17 16:42:18 -0500

    Pre Addon backup
    going to make the start of "Squiggles Core", an addon for my core modules that I use in development. Mainly because I need a custom importer for some of my scene files

--- gogodotjam3-horror-game/Modules/menus/MainMenu.gd
+++ gogodotjam3-horror-game/Modules/menus/MainMenu.gd
@@ -6,6 +6,8 @@ export (PackedScene) var credits_menu : PackedScene
 
 onready var btn_continue := $"GameButtons/VBoxContainer/BtnContinue"
 onready var btn_play := $"GameButtons/VBoxContainer/BtnNewGame"
+onready var anim := $AnimationPlayer
+
 
 func _ready() -> void:
 	if not game_scene_path:
@@ -20,6 +22,7 @@ func _ready() -> void:
 func _on_BtnContinue_pressed() -> void:
 	if game_scene_path:
 		SceneManagement.load_scene(game_scene_path.resource_path, true)
+		anim.play("transition_out")
 
 func _on_BtnNewGame_pressed() -> void:
 	if not game_scene_path:
@@ -27,17 +30,22 @@ func _on_BtnNewGame_pressed() -> void:
 	SaveData.delete_data()
 	yield(VisualServer, "frame_post_draw") # one frame
 	SceneManagement.load_scene(game_scene_path.resource_path, true)
+	anim.play("transition_out")
 
 
 func _on_BtnOptions_pressed() -> void:
 	if options_menu:
 		SceneManagement.load_scene(options_menu.resource_path)
+		anim.play("transition_out")
 
 
 func _on_BtnCredits_pressed() -> void:
 	if credits_menu:
 		SceneManagement.load_scene(credits_menu.resource_path)
+		anim.play("transition_out")
 
 
 func _on_BtnQuit_pressed() -> void:
+	anim.play("transition_out")
+	yield(anim, "animation_finished")
 	get_tree().quit()
